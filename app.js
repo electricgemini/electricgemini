c421286438e0591d6163a6fb1b393b0aed223ad6
Lilian <lil@Jamess-MBP.local>
2017-08-21 13:37:25 -0400

    change cosmic.object.introduction for cosmic.object.about in the content types for index.html

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ app.set('port', (process.env.PORT || 3000))
 app.use('/', express.static(__dirname + '/public/'))
 var Cosmic = require('cosmicjs')
 var bucket_slug = process.env.COSMIC_BUCKET || 'electric-gemini'
-var read_key = process.env.COSMIC_READ_KEY
+var read_key = "QgM72tMuu4JT1eVdZJkKZ2hIoPaJFut45FYTkBHQBi5fwMsQi4"
 app.get('/', function(req, res) {
   Cosmic.getObjects({ bucket: { slug: bucket_slug, read_key: read_key } }, function(err, response) {
     res.locals.cosmic = response;
